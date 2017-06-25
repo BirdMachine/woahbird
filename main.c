401c07b9ebde0b99c633e8761329f6af80178879
birdmachine@blackboxjack.com <birdmachine@blackboxjack.com>
2017-06-24 21:34:27 -0400

    NOW THERE'S NOT JUST ONE SCREEN. BUT TWO!

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "helloWorld.c"
 #include "blankScreen.c"
 #include "corvid.c"
+#include "menu.c"
 
 void init();
 void checkInput();
@@ -23,12 +24,16 @@ void main() {
 
 void init() {
 	
-	DISPLAY_ON;						// Turn on the display
-	set_bkg_data(0, 47, alpha);		// Load 47 tiles into background memory
-	
-	// Use the 'helloWorld' array to write background tiles starting at 0,6 (tile positions)
-	//  and write for 10 tiles in width and 2 tiles in height
-	set_bkg_tiles(0,6,10,2,helloWorld);		
+	DISPLAY_ON;// Turn on the display
+	// Switch to VRAM
+	VBK_REG = 1;
+	// Switch out of VRAM
+	VBK_REG = 0;
+	// Set screen x,y pos to 0,0 and draw the map 20,18(size of the screen)
+	set_bkg_data(0,255,birdTitle_tile_data);
+	set_bkg_tiles(0,0,20,18,birdTitle_map_data);
+	// Show the background
+	SHOW_BKG;	
 
 }
 
@@ -38,23 +43,20 @@ void updateSwitches() {
 	SHOW_BKG;
 }
 
+void menu() {
+	set_bkg_data(0,255,birdWelcomeSM_tile_data);
+	set_bkg_tiles(0,0,20,18,birdWelcomeSM_map_data);
+}
+
 void checkInput() {
 
-    if (joypad() & J_B) {
+    if (joypad() & J_A) {
 		
-		// Use the 'blankScreen' array to write background tiles starting at 0,0 (tile positions)
-		//  and for 20 tiles in width and 18 tiles in height
-		//set_bkg_tiles(0,0,20,18,blankScreen);
-		// Load up the tile data
-		// Switch to VRAM
-		VBK_REG = 1;
-		// Switch out of VRAM
-		VBK_REG = 0;
-		// Set screen x,y pos to 0,0 and draw the map 20,18(size of the screen)
-		set_bkg_data(0,255,bird_tile_data);
-		set_bkg_tiles(0,0,20,18,bird_map_data);
-		// Show the background
-		SHOW_BKG;
+		menu();
+		
     }
 
 }
+
+
+
